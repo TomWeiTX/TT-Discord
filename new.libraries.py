9e5aa6e716115d1ca3e839ba8da48aba19b9ea43
TomWeiTX <146095857+TomWeiTX@users.noreply.github.com>
2023-10-26 10:46:27 +0300

    Create new.libraries.py

---
+++ new.libraries.py
@@ -0,0 +1,14 @@
+import discord
+import interactions
+import requests 
+import sqlite3
+import os
+from ast import alias
+from dis import disco
+from http import client 
+from optparse import Option
+from requests import options
+from discord.ext import commands, tasks
+from discord import guild
+from config import settings
+from keep_alive import keep_alive
